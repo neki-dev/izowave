e24b6bbe7428fdef72519193474f62a0f06fbba9
neki-dev <dev@neki.guru>
2022-06-19 11:57:47 +0300

    Updated game balance

--- src/const/difficulty.ts
+++ src/const/difficulty.ts
@@ -8,11 +8,11 @@ export const PLAYER_ATTACK_DISTANCE_GROWTH = 0.03; // Attack distance growth
 export const PLAYER_ATTACK_PAUSE = 700; // Player attack pause
 export const PLAYER_START_RESOURCES = { bronze: 90, silver: 90, gold: 0 }; // Player start resources
 
-export const WAVE_PAUSE = 25000; // Pause in milliseconds between waves
+export const WAVE_PAUSE = 30000; // Pause in milliseconds between waves
 export const WAVE_ENEMIES_COUNT = 8; // Enemies count on first wave
 export const WAVE_ENEMIES_COUNT_GROWTH = 0.6; // Enemies count growth by wave number
 export const WAVE_ENEMIES_SPAWN_PAUSE = 2300; // Default pause in milliseconds between enemies spawn
-export const WAVE_ENEMIES_SPAWN_PAUSE_GROWTH = -0.01; // Enemies spawn pause growth by wave number
+export const WAVE_ENEMIES_SPAWN_PAUSE_GROWTH = -0.02; // Enemies spawn pause growth by wave number
 export const WAVE_BOSS_SPAWN_RATE = 5; // Every Nth wave a boss will spawn
 export const WAVE_EXPERIENCE = 200; // Gained experience per complete wave
 export const WAVE_EXPERIENCE_GROWTH = 0.1; // Experience count growth by wave number
@@ -34,17 +34,15 @@ export const ENEMY_KILL_EXPERIENCE_GROWTH = 0.07; // Experience growth by wave n
 export const BUILDING_ACTION_RADIUS_GROWTH = 0.3; // Actions radius growth by upgrade
 export const BUILDING_ACTION_PAUSE_GROWTH = -0.1; // Actions pause growth by upgrade
 export const BUILDING_UPGRADE_EXPERIENCE = 30; // Gained experience per upgrade building
-
 export const BUILDING_BUILD_AREA = 180; // Default radius of build area
-export const BUILDING_BUILD_AREA_GROWTH = 0.08; // Radius growth by level
+export const BUILDING_BUILD_AREA_GROWTH = 0.05; // Radius growth by level
+
+export const MINE_RESOURCES_LIMIT = 150; // Maximum amount of resources that mine can
 
 export const TOWER_SHOT_DAMAGE_GROWTH = 0.6; // Shot damage growth by upgrade
 export const TOWER_SHOT_FREEZE_GROWTH = 0.6; // Frozen duration growth by upgrade
 export const TOWER_SHOT_SPEED_GROWTH = 0.3; // Shot speed growth by upgrade
 
-export const MINE_RESOURCES_LIMIT = 150; // Maximum amount of resources that mine can
+export const MEDIC_HEAL_AMOUNT = 10; // Default meedic heal
 
-export const WALL_HEALTH_GROWTH = 0.8; // Wall health growth by upgrade
-
-export const MEDIC_HEAL_AMOUNT = 10; // Default heal amount
-export const MEDIC_HEAL_AMOUNT_GROWTH = 1; // Medic heal growth by upgrade
+export const WALL_HEALTH_AMOUNT = 3000; // Default wall health
