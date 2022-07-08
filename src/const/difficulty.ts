63343106ef59e79b194bec2d3c92cfdd7cb2d5ac
u_m1bq5 <NGaladiy@alfabank.ru>
2022-07-09 01:49:24 +0300

    Updated game balance

--- src/const/difficulty.ts
+++ src/const/difficulty.ts
@@ -10,7 +10,7 @@ export const PLAYER_START_RESOURCES = { bronze: 100, silver: 100, gold: 0 }; // 
 
 export const WAVE_PAUSE = 30000; // Pause in milliseconds between waves
 export const WAVE_ENEMIES_COUNT = 8; // Enemies count on first wave
-export const WAVE_ENEMIES_COUNT_GROWTH = 0.6; // Enemies count growth by wave number
+export const WAVE_ENEMIES_COUNT_GROWTH = 0.5; // Enemies count growth by wave number
 export const WAVE_ENEMIES_SPAWN_PAUSE = 2300; // Default pause in milliseconds between enemies spawn
 export const WAVE_ENEMIES_SPAWN_PAUSE_GROWTH = -0.025; // Enemies spawn pause growth by wave number
 export const WAVE_BOSS_SPAWN_RATE = 5; // Every Nth wave a boss will spawn
@@ -21,9 +21,9 @@ export const EXPERIENCE_TO_NEXT_LEVEL = 300; // Need experience count to next le
 export const EXPERIENCE_TO_NEXT_LEVEL_GROWTH = 0.3; // Experience count growth by level
 
 export const CHEST_EXPERIENCE = 5; // Gained experience per open chest
-export const CHEST_EXPERIENCE_GROWTH = 0.2; // Experience growth by wave number
+export const CHEST_EXPERIENCE_GROWTH = 0.3; // Experience growth by wave number
 export const CHEST_RESOURCES = { bronze: 4, silver: 4, gold: 2 }; // Default resources count in chest
-export const CHEST_RESOURCES_GROWTH = 0.1; // Resources count growth by wave number
+export const CHEST_RESOURCES_GROWTH = 0.2; // Resources count growth by wave number
 
 export const ENEMY_HEALTH_GROWTH = 0.14; // Enemy health growth by wave number
 export const ENEMY_SPEED_GROWTH = 0.058; // Enemy speed growth by wave number
@@ -41,7 +41,7 @@ export const AMMUNITION_AMMO_LIMIT = 200; // Maximum amount of ammo
 export const AMMUNITION_AMMO_UPGRADE = 120; // Amount of ammo added per upgrade
 export const AMMUNITION_LIMIT = 4; // Maximum count ammunition on world (N * (wave / 5))
 
-export const MINE_RESOURCES_LIMIT = 150; // Maximum amount of resources that mine can
+export const MINE_RESOURCES_LIMIT = 160; // Maximum amount of resources that mine can
 export const MINE_RESOURCES_UPGRADE = 100; // Amount of resources added per upgrade
 export const MINE_LIMIT = 4; // Maximum count mines on world (N * (wave / 5))
 
