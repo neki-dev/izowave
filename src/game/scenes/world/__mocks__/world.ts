90e7a2e2c17dec75063c6ad6c5e45e9c6c408c86
neki-dev <dev@neki.guru>
2022-11-10 15:45:09 +0300

    Fixed tests

--- src/game/scenes/world/__mocks__/world.ts
+++ src/game/scenes/world/__mocks__/world.ts
@@ -3,12 +3,14 @@ import { World } from '../world';
 const world = {
   getTimerNow: jest.fn(),
   isTimerPaused: jest.fn(() => false),
+  setTimerPause: jest.fn(),
   spawnEnemy: jest.fn(),
   game: {
     difficulty: 1,
     tutorial: {
-      // step: TutorialStep.NONE,
-      on: jest.fn(),
+      isDisabled: true,
+      beg: jest.fn(),
+      end: jest.fn(),
     },
     analytics: {
       track: jest.fn(),
