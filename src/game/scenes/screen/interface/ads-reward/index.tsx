8010c818bfa41a6a106becc8d9d6b692190faf8e
neki-dev <dev@neki.guru>
2023-10-14 00:21:59 +0200

    Updated ads offer

--- src/game/scenes/screen/interface/ads-reward/index.tsx
+++ src/game/scenes/screen/interface/ads-reward/index.tsx
@@ -5,13 +5,15 @@ import { DIFFICULTY } from '~const/world/difficulty';
 import { progressionLinear } from '~lib/difficulty';
 import { phrase } from '~lib/lang';
 import { Amount } from '~scene/system/interface/amount';
-import { Modal } from '~scene/system/interface/modal';
+import { Button } from '~scene/system/interface/button';
 import { IGame, GameScene, GameFlag } from '~type/game';
 import { SDKAdsType } from '~type/sdk';
 import { IWorld } from '~type/world';
 import { WaveEvents } from '~type/world/wave';
 
-import { Amounts, Text } from './styles';
+import {
+  Amounts, Backdrop, Buttons, Container, Text,
+} from './styles';
 
 export const AdsReward: React.FC = () => {
   const game = useGame<IGame>();
@@ -36,46 +38,64 @@ export const AdsReward: React.FC = () => {
   };
 
   const onWaveComplete = (number: number) => {
-    if (number % DIFFICULTY.ADS_REWARD_FREQUENCY === 0) {
-      const experience = progressionLinear({
-        defaultValue: DIFFICULTY.ADS_REWARD_EXPERIENCE,
-        scale: DIFFICULTY.ADS_REWARD_GROWTH,
-        level: number,
-      });
-      const resources = progressionLinear({
-        defaultValue: DIFFICULTY.ADS_REWARD_RESOURCES,
-        scale: DIFFICULTY.ADS_REWARD_GROWTH,
-        level: number,
-      });
-
-      setAdsOfferOpen(true);
-      setAdsReward({ experience, resources });
+    if (number % DIFFICULTY.ADS_REWARD_FREQUENCY !== 0) {
+      return;
     }
+
+    const experience = progressionLinear({
+      defaultValue: DIFFICULTY.ADS_REWARD_EXPERIENCE,
+      scale: DIFFICULTY.ADS_REWARD_GROWTH,
+      level: number,
+    });
+    const resources = progressionLinear({
+      defaultValue: DIFFICULTY.ADS_REWARD_RESOURCES,
+      scale: DIFFICULTY.ADS_REWARD_GROWTH,
+      level: number,
+    });
+
+    setAdsOfferOpen(true);
+    setAdsReward({ experience, resources });
   };
+
+  useEffect(() => {
+    if (isAdsOfferOpen) {
+      game.pause();
+    } else {
+      game.resume();
+    }
+  }, [isAdsOfferOpen]);
 
   useEffect(() => {
     if (!game.isFlagEnabled(GameFlag.ADS)) {
       return;
     }
 
-    world.wave.on(WaveEvents.START, onDeclineAds);
     world.wave.on(WaveEvents.COMPLETE, onWaveComplete);
 
     return () => {
-      world.wave.off(WaveEvents.START, onDeclineAds);
       world.wave.off(WaveEvents.COMPLETE, onWaveComplete);
     };
   }, []);
 
   return (
     isAdsOfferOpen && (
-      <Modal onConfirm={onConfirmAds} onClose={onDeclineAds}>
-        <Text>{phrase('ADS_OFFER')}</Text>
-        <Amounts>
-          <Amount type="RESOURCES">+{adsReward.resources}</Amount>
-          <Amount type="EXPERIENCE">+{adsReward.experience}</Amount>
-        </Amounts>
-      </Modal>
+      <Backdrop>
+        <Container>
+          <Text>{phrase('ADS_OFFER')}</Text>
+          <Amounts>
+            <Amount type="RESOURCES">+{adsReward.resources}</Amount>
+            <Amount type="EXPERIENCE">+{adsReward.experience}</Amount>
+          </Amounts>
+          <Buttons>
+            <Button view="confirm" size="medium" onClick={onConfirmAds}>
+              {phrase('YES')}
+            </Button>
+            <Button view="decline" size="medium" onClick={onDeclineAds}>
+              {phrase('NO')}
+            </Button>
+          </Buttons>
+        </Container>
+      </Backdrop>
     )
   );
 };
