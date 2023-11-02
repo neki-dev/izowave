95212aba8ff2afb3c3046a82a49a3f7135ac30b6
neki-dev <dev@neki.guru>
2023-11-03 00:07:03 +0300

    Minor fixes

--- src/game/scenes/world/entities/shot/ball/variants/frozen.ts
+++ src/game/scenes/world/entities/shot/ball/variants/frozen.ts
@@ -1,11 +1,12 @@
 import { IWorld } from '~type/world';
-import { ShotBallAudio, ShotParams } from '~type/world/entities/shot';
+import { ShotBallAudio, ShotData, ShotParams } from '~type/world/entities/shot';
 
 import { ShotBall } from '../ball';
 
 export class ShotBallFrozen extends ShotBall {
-  constructor(scene: IWorld, params: ShotParams) {
+  constructor(scene: IWorld, params: ShotParams, data: ShotData = {}) {
     super(scene, params, {
+      ...data,
       audio: ShotBallAudio.FROZEN,
       color: 0x00a1ff,
       glow: true,
