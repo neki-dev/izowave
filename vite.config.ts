5e036fd2afa4b585c94a6d2b0b9a6813477e89f4
Nikita Galadiy <dev@neki.guru>
2025-02-11 12:26:02 +0100

    chore(*): update configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import react from '@vitejs/plugin-react';
-import { reuse } from 'alias-reuse';
 import path from 'path';
+
+import { reuse } from 'alias-reuse';
 import { defineConfig } from 'vite';
-import checkerPlugin from 'vite-plugin-checker';
+import { checker } from 'vite-plugin-checker';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 const tsconfig = path.resolve(__dirname, 'tsconfig.json');
 
 export default defineConfig(({ mode }) => ({
   plugins: [
-    react(),
-    checkerPlugin({
+    checker({
       typescript: true,
     }),
     viteStaticCopy({
